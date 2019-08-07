6847cf8ffa256b5682adc01f3d262c941f555456
leiwen <leiwen@szqgly.com>
2019-08-07 14:08:20 +0800

    feat[springboot]:修改项目结构

--- src/main/java/cn/lxtkj/Application.java
+++ src/main/java/cn/lxtkj/Application.java
@@ -1,4 +1,4 @@
-package cn.lxtkj.app;
+package cn.lxtkj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,16 +6,16 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-//@ComponentScan(basePackages = {"cn.lxtkj.firstappdemo.controller","cn.lxtkj.firstappdemo.service"})
-@ComponentScan("cn.lxtkj.firstappdemo")
-@EnableJpaRepositories("cn.lxtkj.firstappdemo.dao")
+//@ComponentScan(basePackages = {"cn.lxtkj.controller","cn.lxtkj.service"})
+@ComponentScan("cn.lxtkj")
+@EnableJpaRepositories("cn.lxtkj.dao")
 @EnableAutoConfiguration
-@EntityScan("cn.lxtkj.firstappdemo.entity")
+@EntityScan("cn.lxtkj.entity")
 //@EnableScheduling//自动执行
 //标识该接口全部返还json格式
-public class App {
+public class Application {
     public static void main(String[] args){
         //主函数运行Springboot项目
-        SpringApplication.run(App.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
