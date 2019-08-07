475a22635e798366563228f7812e5e07e2866698
leiwen <657106593@qq.com>
2019-08-08 00:17:30 +0800

    feat[java]:新增配置文件

--- springboot/src/main/java/cn/lxtkj/config/WebSecurityConfig.java
+++ springboot/src/main/java/cn/lxtkj/config/WebSecurityConfig.java
@@ -1,6 +1,6 @@
 package cn.lxtkj.config;
 
-import org.sang.service.UserService;
+import cn.lxtkj.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ springboot/src/main/java/cn/lxtkj/mapper/CategoryMapper.java
@@ -0,0 +1,21 @@
+package cn.lxtkj.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import cn.lxtkj.bean.Category;
+
+import java.util.List;
+
+/**
+ * Created by sang on 2017/12/19.
+ */
+@Mapper
+public interface CategoryMapper {
+    List<Category> getAllCategories();
+
+    int deleteCategoryByIds(@Param("ids") String[] ids);
+
+    int updateCategoryById(Category category);
+
+    int addCategory(Category category);
+}
