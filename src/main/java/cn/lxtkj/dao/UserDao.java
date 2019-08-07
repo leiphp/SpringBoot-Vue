1e82e2018f5db5373208dae7cd8809d89e32cf99
leiwen <leiwen@szqgly.com>
2019-08-07 18:56:46 +0800

    fix[springboot]:删除文件

--- src/main/java/cn/lxtkj/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package cn.lxtkj.dao;
-
-import cn.lxtkj.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Integer> {
-}
