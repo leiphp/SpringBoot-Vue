a73f24f543d13cc3acf225091737573636a88eb7
leiwen <leiwen@szqgly.com>
2019-08-09 14:18:00 +0800

    feat[java+vue]:调通vue和springboot登录接口

--- springboot/src/main/java/cn/lxtkj/Application.java
+++ springboot/src/main/java/cn/lxtkj/Application.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 //@ComponentScan(basePackages = {"cn.lxtkj.controller","cn.lxtkj.service"})
 @ComponentScan("cn.lxtkj")
-@EnableJpaRepositories("cn.lxtkj.dao")
+//@EnableJpaRepositories("cn.lxtkj.dao")
 @EnableAutoConfiguration
 @EntityScan("cn.lxtkj.entity")
 @EnableWebSecurity
