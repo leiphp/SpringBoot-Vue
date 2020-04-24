19b0cfa6900d3ce3357408fd9b18b2aadfea9a27
leiwen <leiwen@youmitu.com>
2020-04-24 17:29:35 +0800

    del[清楚分离前端代码]

--- vueadmin/src/utils/utils.js
+++
@@ -1,9 +0,0 @@
-export const isNotNullORBlank = (...args)=> {
-  for (var i = 0; i < args.length; i++) {
-    var argument = args[i];
-    if (argument == null || argument == '' || argument == undefined) {
-      return false;
-    }
-  }
-  return true;
-}
